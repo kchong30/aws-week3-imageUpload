b56d4926bd7c5c7157e5b65f851f746381ccb5d5
Kevin Chong <kchong30@my.bcit.ca>
2023-01-17 14:08:03 -0800

    able to save to database, and view images

--- server.js
+++ server.js
@@ -2,27 +2,31 @@ const express = require('express')
 const app = express()
 const multer = require('multer')
 const upload = multer({ dest: 'images/' })
+const database = require('./database');
+
+require('dotenv').config()
 
 const fs = require('fs')
 
-
-
 app.set('view engine', 'ejs')
 
-app.get('/', (req, res) => {
-  res.render('index')
-})
+app.get("/", async (req, res) => {
+    const images = await database.getImages()
+    res.render("index", { images })
+  })
 
 app.post('/saveImage', upload.single('image'), (req, res) => {
-    // 4
     const imagePath = req.file.path
     const description = req.body.description
-  
-    // Save this data to a database probably
-  
-    console.log(description, imagePath)
+    database.addImage(imagePath, description);
     res.render('savedImage', {description, imagePath})
 })
+
+app.get('/image/:id'), (req,res) => {
+    const id = req.body.id
+    const image = database.getImage(id);
+    res.render('singleImage', {image});
+}
 
 app.get('/images/:imageName', (req, res) => {
     // do a bunch of if statements to make sure the user is 
