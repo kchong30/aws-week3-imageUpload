bc55af7f757cb29d777b4ab4f1cee52758af856b
Kevin Chong <kchong30@my.bcit.ca>
2023-01-30 09:02:52 -0800

    S3

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const app = express()
 const multer = require('multer')
-const upload = multer({ dest: 'images/' })
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
 const database = require('./database');
+const crypto = require ('crypto');
+const s3 = require('./s3.js');
+
+const generateFileName = (bytes = 32) => crypto.randomBytes(bytes).toString('hex')
+
+
 
 require('dotenv').config()
 
@@ -11,15 +18,35 @@ const fs = require('fs')
 app.set('view engine', 'ejs')
 
 app.get("/", async (req, res) => {
-    const images = await database.getImages()
-    res.render("index", { images })
+  const images = await database.getImages()
+
+
+  // Add the signed url to each image
+  for (const image of images) {
+    image.imageURL = await s3.getSignedUrl(image.file_path)
+  }
+
+  res.render("index", {images})
   })
 
-app.post('/saveImage', upload.single('image'), (req, res) => {
-    const imagePath = req.file.path
+app.post('/saveImage', upload.single('image'), async(req, res) => {
+/*     const imagePath = req.file.path
     const description = req.body.description
     database.addImage(imagePath, description);
-    res.render('savedImage', {description, imagePath})
+    res.render('savedImage', {description, imagePath}) */
+      // Get the data from the post request
+  const description = req.body.description
+  const fileBuffer = req.file.buffer
+  const mimetype = req.file.mimetype
+  const fileName = generateFileName()
+
+  // Store the image in s3
+  const s3Result = await s3.uploadImage(fileBuffer, fileName, mimetype)
+
+  // Store the image in the database
+  const databaseResult = await database.addImage(fileName, description)
+
+  res.status(201).send(databaseResult)
 })
 
 app.get('/image/:id'), (req,res) => {
